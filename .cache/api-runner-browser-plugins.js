78fef19f671bbb3089ae618fbb1641612ac03a2b
Charlie Astrada <carlosastrada@gmail.com>
2020-05-12 19:36:26 -0300

    .gitignore fix
    Accidentally created repo without gitignore set up. Lots of generated files were added so this fix removes them.

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,4 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-mdx/gatsby-browser.js'),
-      options: {"plugins":[],"defaultLayouts":{"default":"/Users/charlieastrada/Sites/mango-prototype/src/components/layout.js"},"gatsbyRemarkPlugins":[{"resolve":"gatsby-remark-images"}]},
-    }]
