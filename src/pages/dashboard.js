93b71b4139132c3fecdd5f7ed547e434c6a6c1b8
Charlie Astrada <carlosastrada@gmail.com>
2020-05-14 18:47:30 -0300

    Add redirect to Dashboard/login when user goes to /dashboard
    This is to invoke the login authentication component for the private routing. Right now, we allow dashboard/anything-here but the idea is to create a private section with forced authentication.

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { navigate } from 'gatsby';
 import { Router } from '@reach/router';
 import Layout from '../components/layout';
 import Profile from '../components/profile';
@@ -6,18 +7,27 @@ import RouteBase from '../components/routes/base';
 import RouteSecret from '../components/routes/secret';
 import RouteLogin from '../components/routes/login';
 
-const Dashboard = () => (
-  <>
-    <Layout>
-      <Profile />
-      {/* <h1>Dashboard</h1> */}
-      <Router>
-        <RouteBase path="/dashboard/base" />
-        <RouteSecret path="/dashboard/secret" />
-        <RouteLogin path="/dashboard/login" />
-      </Router>
-    </Layout>
-  </>
-);
+const Dashboard = ({ location }) => {
+  useEffect(() => {
+    if (location.pathname.match(/^\/dashboard\/?$/)) {
+      // Routes any match to login component and replace history to avoid redirect loops when using back/foward buttons
+      navigate('/dashboard/login', { replace: true });
+    }
+  }, []);
+
+  return (
+    <>
+      <Layout>
+        <Profile />
+        {/* <h1>Dashboard</h1> */}
+        <Router>
+          <RouteBase path="/dashboard/base" />
+          <RouteSecret path="/dashboard/secret" />
+          <RouteLogin path="/dashboard/login" />
+        </Router>
+      </Layout>
+    </>
+  );
+};
 
 export default Dashboard;
