c2e9d2f23b5f64bc26a138005b77bd42d616c027
Charlie Astrada <carlosastrada@gmail.com>
2020-05-14 19:35:04 -0300

    Add a redirect in login to /secret if user is authenticated
    We redirect /dashboard to the login but we don't want the person to stay there if they are already logged in.

--- src/components/routes/login.js
+++ src/components/routes/login.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { useIdentityContext } from 'react-netlify-identity-widget';
+import { navigate } from 'gatsby';
 
-const RouteLogin = () => (
-  <>
-    <h1>Log In Or Sign Up</h1>
-    <button>Log In</button>
-  </>
-);
+const RouteLogin = () => {
+  const identity = useIdentityContext();
+
+  if (identity?.isLoggedIn) {
+    navigate('/dashboard/secret', { replace: true });
+  }
+
+  return (
+    <>
+      <h1>Log In Or Sign Up</h1>
+      <button>Log In</button>
+    </>
+  );
+};
 
 export default RouteLogin;
