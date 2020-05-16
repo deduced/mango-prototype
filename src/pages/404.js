2889c723d9fb179c9020563ad726ca2f69c618eb
Charlie Astrada <carlosastrada@gmail.com>
2020-05-16 00:14:13 -0300

    Add a 404

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const PageNotFound = () => (
+  <>
+    <h1>OH SNAP! Something went really wrong! </h1>
+    <h2>HELP ME BABY JESUS!</h2>
+  </>
+);
+
+export default PageNotFound;
